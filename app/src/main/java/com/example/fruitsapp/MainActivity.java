ced851f2712181120b5e3e42e1440a14c78caeb4
Andrey Kosarygin <gaio-gulio-cesare@yandex.ru>
2022-04-01 22:20:44 +0300

    Добавил action bar menu с функционалом добавления и удаления

--- app/src/main/java/com/example/fruitsapp/MainActivity.java
+++ app/src/main/java/com/example/fruitsapp/MainActivity.java
@@ -1,25 +1,55 @@
 package com.example.fruitsapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FruitsDB fruitsDB;
+    private FruitsRecyclerAdapter fruitsRecyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FruitsDB fruitsDB = new FruitsDB();
+        fruitsDB = new FruitsDB();
         LayoutInflater inflater = getLayoutInflater();
-        FruitsRecyclerAdapter fruitsRecyclerAdapter = new FruitsRecyclerAdapter(fruitsDB, inflater);
+        fruitsRecyclerAdapter = new FruitsRecyclerAdapter(fruitsDB, inflater);
         RecyclerView recyclerView = findViewById(R.id.fruits_recycler_view);
         recyclerView.setAdapter(fruitsRecyclerAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar_menu, menu);
+        return true;
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_bar_add:
+            fruitsDB.addFruit("Новый фрукт", "Это новый фрукт", R.drawable.fruits);
+            fruitsRecyclerAdapter.notifyItemInserted(fruitsDB.getDBSize() + 1);
+            return true;
+            case R.id.action_bar_clear:
+                for (int i = 0; i < fruitsDB.getDBSize(); i++) {
+                    fruitsRecyclerAdapter.notifyItemRemoved(0);
+                }
+                fruitsDB.clearDB();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
